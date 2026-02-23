ce14fc272f39c60e9367adc95b02778c3aec0852
laserramica-vaktija <laserramica@gmail.com>
2026-02-23 14:00:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-const CACHE="vaktijausk-v1";
+const CACHE_NAME = "vaktija-cache-v1";
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(cache=>{
-      return cache.addAll(["index.html","manifest.json","ikon.png","sw.js"]);
-    })
-  );
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/manifest.json"
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
 });
